d7dec11950efcace2a0b02fe03d6a77f838f33ea
jeonghyeon kim <mbc5200@gmail.com>
2026-09-16 20:42:49 +0900

    Add chalk example

--- try.js
+++ try.js
@@ -26,5 +26,8 @@
 // README 예제가 const x = require("...") 형태라면 import x from "..." 로 바꿔야 합니다.
 
 // ↓ 여기부터 작성
+import chalk from "chalk";
 
-console.log("아직 비어 있습니다. 위 주석을 읽고 패키지를 하나 import해서 호출해보세요.");
+console.log(chalk.blue("Hello, npm!"));
+console.log(chalk.green.bold("SUCCESS"));
+console.log(chalk.red.bold("ERROR"));
